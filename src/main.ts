fb32a8801fdf0e3a2509cf01e17894555d6ee870
smurawczik <sebastian.murawczik@gmail.com>
2024-04-13 13:36:47 +0200

    add some helpers and security helpers

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,17 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { seedFakeData } from './database/seed';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import * as compression from 'compression';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+  app.use(cookieParser());
+  app.use(compression());
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
 
   const configService = app.get(ConfigService);
